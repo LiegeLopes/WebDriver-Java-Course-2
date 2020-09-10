20c27c15b8392ad3e7885e49747eaf2e9b8b11f9
LiegeLopes <34957712+LiegeLopes@users.noreply.github.com>
2020-09-10 17:19:37 -0300

    Add files via upload

--- InformacoesUsuarioTest.java
+++ InformacoesUsuarioTest.java
@@ -1,127 +1,109 @@
-package Tests;
+package tests;
 
+import static org.junit.Assert.*;
+
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import suporte.Generator;
+import suporte.Screenshot;
+import suporte.Web;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.assertEquals;
-
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "InformacoesUsuarioTestData.csv")
 public class InformacoesUsuarioTest {
-    private WebDriver driver;
+    private WebDriver navegador;
+
+    @Rule
+    public TestName test = new TestName();
 
     @Before
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver", "/home/usertqi/Drivers/chromedriver");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--no-sandbox");
+    public void setUp() {
+        navegador = Web.createChrome();
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        // Clicar no link que possui o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
 
-        driver.get("http://www.juliodelima.com.br/taskit/");
+        // Identificando o formulário de Login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
 
-        //clicar no link que possui o texto Sign in
-        driver.findElement(By.linkText("Sign in")).click();
-
-        //identificando o formuário de login
-        WebElement formularioSignInBox = driver.findElement(By.id("signinbox"));
-
-        //Digitar no campo com name login que está dentro do formulário id signinbox o texto "julio0001"
+        // Digitar no campo com name "login" que está dentro do formulário de id "signinbox" o texto "julio0001"
         formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
 
-        //Digitar no campo com name password que está dentro do formulário id signinbox o texto "123456"
+        // Digitar no campo com name "password" que está dentro do formulário de id "signinbox" o texto "123456"
         formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
 
-        //clicar no link com o texto "SIGN IN"
-        driver.findElement(By.linkText("SIGN IN")).click();
+        // Clicar no link com o texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
 
-            /*validar que dentro do elemento class "me" está o texto "Hi, Júlio"
-            WebElement me = driver.findElement(By.className("me"));
-            String textoNoElementoMe = me.getText();
-            assertEquals("Hi, Julio", textoNoElementoMe);*/
+        // Clicar em um link que possui a class "me"
+        navegador.findElement(By.className("me")).click();
 
-        //clicar em um link que possui a class me
-        driver.findElement(By.className("me")).click();
-
-        //clicar em um link que possui "more data about"
-        driver.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+        // Clicar em um link que poossui o texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
     }
 
-    private Object InterruptedException;
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(@Param(name="tipo")String tipo, @Param(name="contato")String contato, @Param(name="mensagem")String mensagemEsperada) {
+        // Clicar no botão através do seu xpath //button[@data-target="addmoredata"]
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
 
-    //@Test
-    public void AdicionarInformaçaoAdicionalDoUsuario() throws InterruptedException {
+        // Identificar a popup onde está o formulário de id addmoredata
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
 
-            //clicar no botão através do seu xpath //button[@data-target="addmoredata"]
-            driver.findElement(By.xpath("//button[@data-target='addmoredata']")).click();
+        // Na combo de name "type" escolhe a opção "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+        new Select(campoType).selectByVisibleText(tipo);
 
-            //identificar a popup onde está o formulário "addmoredata"
-            WebElement popupAddMoreData = driver.findElement(By.id("addmoredata"));
+        // No campo de name "contact" digitar "+5511999999999"
+        popupAddMoreData.findElement(By.name("contact")).sendKeys(contato);
 
-            //no combo de name type escolha a opção "Phone"
-            WebElement campoType = popupAddMoreData.findElement(By.name("type"));
-            new Select(campoType).selectByVisibleText("Phone");
+        // Clicar no link de text "SAVE" que está na popup
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
 
-            //no campo de name "contact" digitar "+5534999999999"
-            popupAddMoreData.findElement(By.name("contact")).sendKeys("+5534999999999");
-
-            //clicar no link "SAVE" que está no popup
-            popupAddMoreData.findElement(By.linkText("SAVE")).click();
-
-            //na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
-            WebElement mensagemPopup = driver.findElement(By.id("toast-container"));
-            String mensagem = mensagemPopup.getText();
-            assertEquals("Your contact has been added!", mensagem);
-
-
-
-            Thread.sleep(1000);
-
-            if (driver.getPageSource().contains("I'm Feeling Lucky")) {
-                System.out.println("Pass");
-            } else {
-                System.out.println("Fail");
-            }
-
-        }
-        @Test
-        public void removerUmContatoDeUmUsuario(){
-            // clicar no elemento com xpath //*[@id="moredata"]/div[1]/ul/li[1]/a/i
-            driver.findElement(By.xpath("//*[@id=\"moredata\"]/div[1]/ul/li[1]/a/i")).click();
-
-            // confirmar a janela javascript
-            driver.switchTo().alert().accept();
-
-            //validar que a mensagem apresentada foi "Rest in peace, dear phone!"
-            WebElement mensagemPopup = driver.findElement(By.id("toast-container"));
-            String mensagem = mensagemPopup.getText();
-            assertEquals("Rest in peace, dear phone!", mensagem);
-
-            //Aguardar até 10 segundos para que a janela desapareça
-            WebDriverWait aguardar = new WebDriverWait(driver,10);
-            aguardar.until(ExpectedConditions.stalenessOf(mensagemPopup));
-
-            //Clicar no link com texto logout
-            driver.findElement(By.linkText("Logout")).click();
-
-        }
-
-        @After
-    public void teardown(){
-            driver.quit();
-        }
+        // Na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals(mensagemEsperada, mensagem);
     }
 
+    @Test
+    public void removerUmContatoDeUmUsuario(@Param(name="telefone")String telefone, @Param(name="mensagem")String mensagemEsperada) {
+        // Clicar no elemento pelo seu xpath //span[text()="+551133334444"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()=\"" + telefone + "\"]/following-sibling::a")).click();
+
+        // Confirmar a janela javascript
+        navegador.switchTo().alert().accept();
+
+        // Validar que a mensagem apresentada foi Rest in peace, dear phone!
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals(mensagem, mensagem);
+
+        String screenshotArquivo = "/Users/julio.lima/test-report/taskit/" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
+        Screenshot.tirar(navegador, screenshotArquivo);
+
+        // Aguardar até 10 segundos para que a janela desapareça
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
+
+        // Clicar no link com texto "Logout"
+        navegador.findElement(By.linkText("Logout")).click();
+    }
+
+    @After
+    public void tearDown() {
+        // Fechar o navegador
+        navegador.quit();
+    }
+}
